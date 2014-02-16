9f35f66f529c3c35db83811553362a5a3e8dc55c
vova <jaromudr@gmail.com>
2014-02-16 13:55:00 +0200

    Dombuilder created

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-dombuilder
-==========
+Dombuilder
+=========
+
+dom(selector, [attributes, [nested]])
+
+Features
+------
+
+  - Build DOM tree with javascript
+  
+```
+var element = dom("div", {"class": "frame-choice", "data-frame-src": this.imageSrc}, [
+    dom("img", {src: this.thumbSrc, height: 100})
+]);
+
+document.body.appendChild(element);
+```
+
+  - Use css selectors
+ 
+```
+dom("div")                                  // <div></div>
+dom("img#id")                               // <img id="id" />
+dom(#id.class1.class2)                      // <div id="id" class="class1 class2"></div>
+```
+
+  - Nested elements
+
+```
+dom("ul", {}, [                             // <ul>
+    dom("li", {innerHTML: "First"}),        //      <li>First</li>
+    dom("li", {innerHTML: "Second"}),       //      <li>Second</li>
+    dom("li", {innerHTML: "Third"})         //      <li>Third</li>
+])                                          // </ul>
+```    
---
+++ dombuilder.js
@@ -0,0 +1,35 @@
+(function(global) {
+
+    function dom(query, attrs, childs){
+        var match = query.match(/^([^#.]*)#?([^.$]*).?(\S*)/),
+            type = match[1]||"div",
+            id = match[2]
+            classes = match[3].replace(/\./g, " ");
+
+        var element = document.createElement(type),
+            key;
+
+        if(attrs){
+            for(key in attrs){
+                if(attrs.hasOwnProperty(key)){
+                    if(key === "id" || key === "innerHTML" || key === "value" || key === "src" || key === "className"){
+                        element[key] = attrs[key];
+                    }else{
+                        element.setAttribute(key, attrs[key]);
+                    }
+                }
+            }
+        }
+
+        id && element.setAttribute("id", id);
+        classes && element.setAttribute("class", classes);
+
+        childs && childs.forEach(function(child){
+           element.appendChild(child);
+        });
+        return element;
+    };
+
+    global.dom = dom;
+
+})(window);
