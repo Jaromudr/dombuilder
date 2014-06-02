7eb1588db4c4b7bb3b38a13e244adba0cecf5fcd
Vladimir Myskov <jaromudr@gmail.com>
2014-06-02 19:54:12 +0300

    Improve API

--- dombuilder.js
+++ dombuilder.js
@@ -9,16 +9,25 @@
         var element = document.createElement(type),
             key;
 
-        if(attrs){
-            for(key in attrs){
-                if(attrs.hasOwnProperty(key)){
-                    if(key === "id" || key === "innerHTML" || key === "value" || key === "src" || key === "className"){
-                        element[key] = attrs[key];
-                    }else{
-                        element.setAttribute(key, attrs[key]);
+        if(attrs && attrs.constructor === Array) {
+            childs = attrs;
+            attrs = null;
+        }
+
+        if(attrs !== undefined){
+            if(typeof attrs == 'string' || typeof attrs == "number") {
+                element.innerHTML = attrs;
+            } else {
+                for(key in attrs){
+                    if(attrs.hasOwnProperty(key)){
+                        if(key === "id" || key === "innerHTML" || key === "value" || key === "src" || key === "className"){
+                            element[key] = attrs[key];
+                        }else{
+                            element.setAttribute(key, attrs[key]);
+                        }
                     }
                 }
-            }
+            }            
         }
 
         id && element.setAttribute("id", id);
